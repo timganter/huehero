4f71d5cdb6bd8bc24842d78e6890efb77d7bd4df
Tim Ganter <tim@studiotenfour.com>
2016-10-04 12:48:02 -0700

    Some variable clean up.

--- js/Space.js
+++ js/Space.js
@@ -12,25 +12,30 @@ function Space() {
 }
 
 Space.prototype.make = function() {
-    for(var i=0; i < this.numberOfStars; i++) {
+    var numberOfStars = this.numberOfStars;
+    var numberOfGalaxies = this.numberOfGalaxies;
+    
+    for(var i=0; i < numberOfStars; i++) {
         this.star();
     }    
 
-    for(var i=0; i < this.numberOfGalaxies; i++) {
+    for(var i=0; i < numberOfGalaxies; i++) {
         this.galaxy();
     }
 };
 
 Space.prototype.star = function(x=null, y=null) {
     var ctx = this.ctx;
+    var canvas = this.canvas;
+
     var r = Util.randomNumber(this.maxStarSize);
 
     if (x === null) {
-        var x = Util.randomNumber(this.canvas.width);    
+        var x = Util.randomNumber(canvas.width);    
     }
 
     if (y === null) {
-        var y = Util.randomNumber(this.canvas.height);
+        var y = Util.randomNumber(canvas.height);
         ctx.beginPath();
     }
 
@@ -42,21 +47,20 @@ Space.prototype.star = function(x=null, y=null) {
 };
 
 Space.prototype.galaxy = function() {
-    var ctx = this.ctx;
     var x = Util.randomNumber(this.canvas.width);
     var y = Util.randomNumber(this.canvas.height);
     var xRad = Util.randomNumber(this.maxGalaxySize.xRad);
     var yRad = Util.randomNumber(this.maxGalaxySize.yRad);
 
-    this.galaxyStars(ctx, x, y, xRad, yRad);
+    this.galaxyStars(x, y, xRad, yRad);
 };
 
-Space.prototype.galaxyStars = function(ctx, x, y, xRad, yRad) {
+Space.prototype.galaxyStars = function(x, y, xRad, yRad) {
+    var ctx = this.ctx;
     var xGalaxyCenter = x;
     var yGalaxyCenter = y;
-    var xAmount = null;
-    var yAmount = null;
-
+    var xAmount, yAmount;
+    
     for(var i=0; i < this.starsPerGalaxy; i++) {
         xAmount = Util.randomNumber(xRad);
         if (Util.randomNumber(2) === 0) { xAmount = -Math.abs(xAmount); }
